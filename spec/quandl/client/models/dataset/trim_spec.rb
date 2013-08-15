01139fa221923eca4a8ee12ec32679400bc3b11f
Blake Hilscher <blake@hilscher.ca>
2013-08-15 16:22:51 -0400

    add spec to test Dataset#trim_*

---
+++ spec/quandl/client/models/dataset/trim_spec.rb
@@ -0,0 +1,37 @@
+# encoding: utf-8
+require 'spec_helper'
+
+describe Dataset do
+  
+  let(:source){ create(:source) }
+  subject{ build(:dataset, source_code: source.code, data: Quandl::Data::Random.table(rows: 20, columns: 2) ) }
+  
+  describe "#from_date" do
+    context "before_save" do
+      it "should be nil" do
+        subject.from_date.should be_nil
+      end
+    end
+    context "after_save" do
+      before(:each){ subject.save }
+      it "should equal the last date" do
+        subject.from_date.should eq subject.data_table.to_date[-1][0].to_s
+      end
+    end
+  end
+  
+  describe "#to_date" do
+    context "before_save" do
+      it "should be nil" do
+        subject.to_date.should be_nil
+      end
+    end
+    context "after_save" do
+      before(:each){ subject.save }
+      it "should equal the first date" do
+        subject.to_date.should eq subject.data_table.to_date[0][0].to_s
+      end
+    end
+  end
+  
+end
