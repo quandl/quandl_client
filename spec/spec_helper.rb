8c142707404d030ee345932a686777eb4be00078
Blake Hilscher <blake@hilscher.ca>
2013-09-23 13:00:07 -0400

    move specs into lib/

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,8 +10,8 @@ Dir.glob( factory_dir ).each{|f| require(f); puts f }
 require "quandl/client"
 
 include Quandl::Client
-Quandl::Client.use 'http://localhost:3000/api/'
-# Quandl::Client.use 'http://staging.quandl.com/api/'
+# Quandl::Client.use 'http://localhost:3000/api/'
+Quandl::Client.use 'http://staging.quandl.com/api/'
 Quandl::Client.token = ENV['QUANDL_AUTH_TOKEN']
 
 RSpec.configure do |config|
