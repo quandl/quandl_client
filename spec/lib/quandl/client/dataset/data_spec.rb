24fc43c5c3340326efaea4e979be93b3620733be
Blake Hilscher <blake@hilscher.ca>
2013-09-26 16:55:41 -0400

    data_spec sleeps to ensure timestamps have time to differ

--- spec/lib/quandl/client/dataset/data_spec.rb
+++ spec/lib/quandl/client/dataset/data_spec.rb
@@ -4,36 +4,33 @@ require 'spec_helper'
 describe Dataset do
 
   let(:dataset){ 
-    d = create(:dataset, source_code: "QUANDL_CLIENT_TEST_SOURCE", data: Quandl::Fabricate::Data::Table.rand( rows: 10, columns: 4 ) ) 
-    Dataset.find( d.id )
+    create(:dataset, source_code: "QUANDL_CLIENT_TEST_SOURCE", data: Quandl::Fabricate::Data::Table.rand( rows: 10, columns: 4 ) )
   }
-  let(:previous_attributes){ dataset.attributes.clone }
-
-  before(:each){ 
-    dataset
-    previous_attributes 
-  }
-  
+    
   describe "#data" do
-    subject{ dataset.data }
+    subject{ Dataset.find( dataset.id ).data }
     its(:count){ should eq 10 }
   end
   
   context "updated" do
     
-    subject{ dataset.save; Dataset.find(dataset.id) }
+    subject{
+      dataset
+      sleep(0.5)
+      Dataset.find( dataset.id )
+    }
     
     describe "#data" do
-      before(:each){ dataset.data = Quandl::Fabricate::Data::Table.rand( rows: 12, columns: 4 ) }
-      its(:updated_at){ should_not eq previous_attributes['updated_at'] }
-      its(:data){ should_not eq previous_attributes['data'] }
-      its(:refreshed_at){ should_not eq previous_attributes['refreshed_at'] }
+      before(:each){ subject.data = Quandl::Fabricate::Data::Table.rand( rows: 12, columns: 4 ); subject.save }
+      its(:updated_at){ should_not eq dataset.updated_at }
+      its(:data){ should_not eq dataset.data }
+      its(:refreshed_at){ should_not eq dataset.refreshed_at }
     end
   
     context "#column_spec" do
-      before(:each){ dataset.column_spec = "[0,[\"Date \\n\",{}],[\"Column 1 \",{}],[\"New Column Name \",{}]]" }
-      its(:updated_at){ should_not eq previous_attributes['updated_at'] }
-      its(:column_spec){ should_not eq previous_attributes['column_spec'] }
+      before(:each){ subject.column_spec = "[0,[\"Date \\n\",{}],[\"Column 1 \",{}],[\"New Column Name \",{}]]"; subject.save }
+      its(:updated_at){ should_not eq dataset.updated_at }
+      its(:column_spec){ should_not eq dataset.column_spec }
     end
   
   end
