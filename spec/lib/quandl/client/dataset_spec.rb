44f592756abdbeebf9c4a19888a21fce730a2f8d
Blake Hilscher <blake@hilscher.ca>
2013-11-27 13:04:27 -0500

    Dataset.query.all.metadata should ...

--- spec/lib/quandl/client/dataset_spec.rb
+++ spec/lib/quandl/client/dataset_spec.rb
@@ -15,10 +15,18 @@ describe Dataset do
   end
   
   describe ".query" do
-    let(:datasets){ Quandl::Client::Dataset.query('water').to_a }
+    let(:datasets){ Quandl::Client::Dataset.query('oil').all }
     subject{ datasets }
     its(:first){ should be_a Quandl::Client::Dataset }
-    its(:count){ should eq 16 }
+    
+    describe "#metadata" do
+      subject{ OpenStruct.new(datasets.metadata) }
+      its(:total_count){ should > 1 }
+      its(:per_page){ should eq 20 }
+      its(:sources){ should be_present }
+      its(:status){ should eq 200 }
+      its(:current_page){ should eq 1 }
+    end
   end
   
 end
