2c2209cae6136e9c59ae0b08e05a41a3977ec376
Blake Hilscher <blake@hilscher.ca>
2014-01-20 13:48:32 -0500

    revise example with comments

--- examples/create.rb
+++ examples/create.rb
@@ -1,14 +1,18 @@
 $:.unshift File.join(File.dirname(__FILE__), *%w[.. lib])
 
-require 'pry'
+# load quandl api client
 require "quandl/client"
+
+# tools for debugging
 require "quandl/fabricate"
 require 'securerandom'
+require 'pry'
 
+# Make Dataset available in global namespace
 include Quandl::Client
 
+# configure quandl client
 Quandl::Client.use ENV['QUANDL_API_HOST']
 Quandl::Client.token = ENV['QUANDL_USER_TOKEN']
-# Quandl::Client.use 'http://quandl.com/api/'
 
 binding.pry
