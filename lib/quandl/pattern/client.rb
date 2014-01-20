f7453227156094512a3b433ebd2e414f0566f90a
Blake Hilscher <blake@hilscher.ca>
2014-01-20 14:34:47 -0500

    revise regexp for QUGC-28

--- lib/quandl/pattern/client.rb
+++ lib/quandl/pattern/client.rb
@@ -2,7 +2,7 @@ module Quandl
 class Pattern
   
   define_pattern :dataset_date, /[0-9]{4}\-[0-9]{2}\-[0-9]{2}/, example: "yyyy-mm-dd"
-  define_pattern :code, /[A-Z0-9_]+/, example: 'ALPHA_NUMERIC_1024'
+  define_pattern :code, /[A-Z0-9_]{3,}+/, example: 'ALPHA_NUMERIC_1024'
   define_pattern :full_code, /(#{code}\/)?(#{code})/, example: "(SOURCE_CODE/)#{code.to_example}"
   
 end
