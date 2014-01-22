62e2e6cccbdb9cc173315a437e4e34eedb7ad860
Blake Hilscher <blake@hilscher.ca>
2014-01-22 17:11:49 -0500

    example

--- examples/search.rb
+++ examples/search.rb
@@ -5,8 +5,7 @@ require "quandl/client"
 
 include Quandl::Client
 
-Quandl::Client.use ENV['QUANDL_API_HOST']
-# Quandl::Client.use 'http://staging.quandl.com/api/'
+Quandl::Client.use ENV['QUANDL_TEST_URL']
 Quandl::Client.token = ENV['QUANDL_TEST_TOKEN']
 
 datasets = Dataset.query('water').all
