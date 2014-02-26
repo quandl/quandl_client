b121d5131a3bbfe4a74cb6afaf87c6430eaa9c3c
Blake Hilscher <blake@hilscher.ca>
2014-02-26 18:34:09 -0500

    add scraper= which takes a file_path and converts it to a faraday multipart upload

--- lib/quandl/client/models/scraper.rb
+++ lib/quandl/client/models/scraper.rb
@@ -3,9 +3,13 @@ module Client
 
 class Scraper < Quandl::Client::Base
   
-  attributes :id, :name, :scraper_url, :git_url, :git_reference, :created_at, :updated_at, :type
+  attributes :id, :name, :scraper, :scraper_url, :git_url, :git_reference, :created_at, :updated_at, :type
   
   validates :name, presence: true
+  
+  def scraper=(value)
+    write_attribute(:scraper, Faraday::UploadIO.new(value, 'text/plain') )
+  end
   
 end
 
