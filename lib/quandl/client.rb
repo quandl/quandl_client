b959a995801588704a4bb43ddb8d8c62ad57f7b1
Blake Hilscher <blake@hilscher.ca>
2014-03-12 17:10:46 -0400

    QUGC-55 Add Report for filing reports

--- lib/quandl/client.rb
+++ lib/quandl/client.rb
@@ -17,6 +17,7 @@ require 'quandl/client/models/source'
 require 'quandl/client/models/user'
 require 'quandl/client/models/location'
 require 'quandl/client/models/scraper'
+require 'quandl/client/models/report'
 
 module Quandl
   module Client
