bafa0c61b79134b734439d9a81b6d735c8dc93bf
Blake Hilscher <blake@hilscher.ca>
2014-03-19 17:26:00 -0400

    QUGC-123 Add support for supersets.

--- lib/quandl/client/models/superset.rb
+++ lib/quandl/client/models/superset.rb
@@ -6,4 +6,35 @@ class Quandl::Client::Superset < Quandl::Client::Base
   attributes :column_codes, :column_names
   attributes :frequency, :from_date, :to_date
   
+  validates :code, presence: true, format: { with: Quandl::Pattern.code, message: "is invalid. Expected format: #{Quandl::Pattern.code.to_example}" }
+  validates :column_codes, presence: true
+  validate :column_codes_should_be_valid!
+  
+  def data
+    @data ||= Quandl::Client::Dataset::Data.with_id(id)
+  end
+  
+  protected
+  
+  def column_codes_should_be_valid!
+    # must be an array
+    unless column_codes.respond_to?(:each)
+      # failure
+      self.errors.add( :column_codes, "expected an array, but got #{column_codes.class}.")
+      # nothing more to do
+      return false
+    end
+    # check each code
+    column_codes.each do |code|
+      # is the code valid?
+      next if code =~ /#{Quandl::Pattern.code}\.#{Quandl::Pattern.code}\.[0-9]+/
+      # otherwise report error
+      self.errors.add( :column_codes, "Code '#{code}' is invalid. Expected: /#{Quandl::Pattern.code.to_example}.#{Quandl::Pattern.code.to_example}.INTEGER/" )
+      # nothing more to do here
+      return false
+    end
+    # success
+    true
+  end
+  
 end
