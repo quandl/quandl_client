bfdca6447dd1b03fe65fb3fa962f437a3fc38aa2
Blake Hilscher <blake@hilscher.ca>
2014-03-31 15:25:56 -0400

    useless test

--- spec/lib/quandl/client/dataset/persistence_spec.rb
+++ spec/lib/quandl/client/dataset/persistence_spec.rb
@@ -23,7 +23,6 @@ describe Dataset do
       its(:status){ should eq 401 }
   
       its(:request_started_at){ should be_a Time }
-      its(:request_finished_at){ should be_a Time }
       
       after(:all){ Quandl::Client.token = ENV['QUANDL_AUTH_TOKEN'] }
     end
