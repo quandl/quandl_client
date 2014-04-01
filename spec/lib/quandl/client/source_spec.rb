8b73255ada66e8efbda44c2e4b1cefb2bebd3175
Blake Hilscher <blake@hilscher.ca>
2014-04-01 14:53:20 -0400

    normal users can create sources

--- spec/lib/quandl/client/source_spec.rb
+++ spec/lib/quandl/client/source_spec.rb
@@ -26,7 +26,7 @@ describe Source do
   
   context "with token" do
 
-    before(:all){ Quandl::Client.token = ENV['QUANDL_AUTH_TOKEN'] }
+    before(:all){ Quandl::Client.token = ENV['QUANDL_USER_TOKEN'] }
     
     describe "#save" do
       
