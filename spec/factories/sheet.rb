baab869f14adb3f6f84eee22b82f401589aa464b
Rakib <rakib@quandl.com>
2014-09-30 12:53:52 -0400

    redirect_path attribute added to sheet model

--- spec/factories/sheet.rb
+++ spec/factories/sheet.rb
@@ -3,6 +3,7 @@ FactoryGirl.define do
   factory :sheet do
     sequence(:title) { |n| "Test sheet #{n} #{(Time.now.to_f * 1000).to_i}" }
     description{ "Test sheet description."}
+    redirect_path nil
   end
 
 end
